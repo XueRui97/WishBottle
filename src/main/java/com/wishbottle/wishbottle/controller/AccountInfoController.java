137362b0765d37dd852e1943aff0362b288955f2
jakejian <weijiancheng999@163.com>
2018-06-25 16:28:26 +0800

    后台管理 controller

--- src/main/java/com/wishbottle/wishbottle/controller/AccountInfoController.java
+++ src/main/java/com/wishbottle/wishbottle/controller/AccountInfoController.java
@@ -3,19 +3,69 @@ package com.wishbottle.wishbottle.controller;
 
 
 import com.wishbottle.wishbottle.bean.AccountInfo;
+import com.wishbottle.wishbottle.service.AccountInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
 public class AccountInfoController {
+    @Autowired
+    private AccountInfoService accountInfoService;
     @GetMapping()
-    public String  index(){
-        AccountInfo accountInfo=new AccountInfo();
+    public String  first(){
+        //add
+       /* AccountInfo accountInfo=new AccountInfo();
         accountInfo.setNikeName("13");
         accountInfo.setAvatar("1asdas");
         accountInfo.setEmail("1231@qq.com");
         accountInfo.setPassword("123qweqwe");
         accountInfo.setSelfIntro("1231aweqweqwe");
+        Date d = new Date();
+        //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss ");
+        accountInfo.setRegestTime(d);
+        accountInfoService.addAccountInfo(accountInfo);
+    */
+        //find
+        List<AccountInfo> accountInfoList=accountInfoService.queryByAccountName("12","12");
+        if(accountInfoList.isEmpty()){
+            System.out.print("查不到");
+        }
+        else {
+            AccountInfo accountInfo1 = accountInfoList.get(0);
+            System.out.print(accountInfo1.getAccountID());
+        }
         return "index";
     }
-
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+   @GetMapping("/logPage")
+    public String log(){
+        return "logPage";
+   }
+    @GetMapping("/accountPage")
+    public String account(){
+        return "accountPage";
+    }
+    @GetMapping("/wishPage")
+    public String wish(){
+        return "wishPage";
+    }
+    @GetMapping("/commentPage")
+    public String comment(){
+        return "commentPage";
+    }
+    @GetMapping("/collectPage")
+    public String collect(){
+        return "collectPage";
+    }
 
 }
